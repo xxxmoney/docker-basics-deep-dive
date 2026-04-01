b1a54d00c14f72133c4c5791ae907cffff62ef7f
jakub.hana <jakub.hana@sprinx.com>
2026-04-01 15:25:32 +0200

    Add dockerignore, remove container before running when stopped

--- server/run.bat
+++ server/run.bat
@@ -3,6 +3,10 @@
 :: Running the .dockerfile image
 ::
 
+:: To make this runnable again, we firstly remove any existing container with the same name, if it exists
+:: We will ommit the `-f` parameter, which forces the removal (now it only remove the stopped container)
+docker rm "thoughts-api-01"
+
 :: As we have built the image, we can run it as container with `docker run`
 :: With parameters:
 :: - `--name` - to give the container a name
@@ -10,6 +14,7 @@
 :: - `-p` - for mapping of ports - `PORT_LOCAL:PORT_CONTAINER`
 :: - and the name of the image as last
 docker run -d --name "thoughts-api-01" -p "8080:666" "thoughts-api"
+:: BTW - there is also an option to include `--rm` parameter, which would remove the container after it stops
 
 :: We can also list running containers (or even stopped)
 :: `docker ps` - lists running containers
