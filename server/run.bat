35556297be253e2292e2748474b99577cd944a75
jakub.hana <jakub.hana@sprinx.com>
2026-04-07 15:35:14 +0200

    Functional thoughts getting/saving, include volume for persistance

--- server/run.bat
+++ server/run.bat
@@ -5,7 +5,7 @@
 
 :: To make this runnable again, we firstly remove any existing container with the same name, if it exists
 :: We will ommit the `-f` parameter, which forces the removal (now it only remove the stopped container)
-docker rm "thoughts-api-01"
+:: docker rm "thoughts-api-01"
 
 :: As we have built the image, we can run it as container with `docker run`
 :: With parameters:
@@ -13,7 +13,7 @@ docker rm "thoughts-api-01"
 :: - `-d` - detached mode, meaning the container will run in the background
 :: - `-p` - for mapping of ports - `PORT_LOCAL:PORT_CONTAINER`
 :: - and the name of the image as last
-docker run -d --name "thoughts-api-01" -p "8080:666" "thoughts-api"
+docker run -d --rm --name "thoughts-api-01" -p "8080:666" -v "thoughts-api-data:/app/data" "thoughts-api"
 :: BTW - there is also an option to include `--rm` parameter, which would remove the container after it stops
 
 :: Regarding the `-v` volume parameter:
