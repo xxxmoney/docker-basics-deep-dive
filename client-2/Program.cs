5445009583b601e15bb3074905e5c30de1db0ccb
jakub.hana <jakub.hana@sprinx.com>
2026-04-30 13:50:29 +0200

    Create client 2 .NET project, gitignore

---
+++ client-2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// TODO: call the server api
+
+using System.Globalization;
+
+Console.WriteLine("Test");
+
+// var url = Environment.GetEnvironmentVariable("API_URL");
+// using var client = new HttpClient() { BaseAddress = new Uri(url) };
+//
+// async Task<List<string>> GetThoughts() {
+//   var response = await client.GetAsync($"/api/values");
+//
+//
+// }
+//
